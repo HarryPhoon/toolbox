2eade28a011854a148931e8706dbb182f0985224
Harry Míchal <harrymichal@seznam.cz>
2019-11-06 10:20:08 +0100

    Add basic reimplementation of 'list' cmd
    TODO:
    - fix listing of images when an image doesn't have a name
    - highlighting of running containers
    - converting to human readable format creation time of images
    
    When an image doesn't have a name, the function fails, because the
    sorting functions freaks out. Listing of just containers is alright.
    
    Highlighting is a tough one because of the way the table of images and
    containers is created. We can add the shell code for the color but that
    messes with the formatting. One solution is to wrap all the text with
    some color and another is to use a different table formatting/rendering
    solution.
    
    Creation time conversion shouldn't be too hard to fix, but I would maybe
    poke the podman guys about this, because they are slightly inconsistent
    when it comes to comparing their standard output vs json. Also there are
    differences in format between images and containers (cmds `images` and
    `ps`)

--- cmd/list.go
+++ cmd/list.go
@@ -16,17 +16,20 @@ limitations under the License.
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
+	"os/exec"
+	"text/tabwriter"
 
 	"github.com/spf13/cobra"
+	"github.com/containers/toolbox/utils"
 )
 
-var (
-	listFlags struct {
-		onlyContainers bool
-		onlyImages     bool
-	}
-)
+var listFlags struct {
+	listContainers bool
+	listImages     bool
+}
 
 var listCmd = &cobra.Command{
 	Use:   "list",
@@ -40,12 +43,128 @@ func init() {
 	rootCmd.AddCommand(listCmd)
 
 	flags := listCmd.Flags()
-	flags.BoolVarP(&listFlags.onlyContainers, "containers", "c", false, "List only toolbox containers, not images")
-	flags.BoolVarP(&listFlags.onlyImages, "images", "i", false, "List only toolbox images, not containers")
+	flags.BoolVarP(&listFlags.listContainers, "containers", "c", false, "List only toolbox containers, not images")
+	flags.BoolVarP(&listFlags.listImages, "images", "i", false, "List only toolbox images, not containers")
 }
 
-func list(cmd *cobra.Command, args []string) {
-	fmt.Println("function 'list'")
-	fmt.Println("OnlyImages:", listFlags.onlyImages)
-	fmt.Println("OnlyContainers:", listFlags.onlyContainers)
+func list(cmd *cobra.Command, args []string) error {
+	if !listFlags.listContainers && !listFlags.listImages {
+		listFlags.listContainers = true
+		listFlags.listImages = true
+	}
+
+	var images []map[string]interface{}
+	var containers []map[string]interface{}
+	var err error
+
+	if listFlags.listImages {
+		images, err = getImages()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if listFlags.listContainers {
+		containers, err = getContainers()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	err = outputList(images, containers)
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
+}
+
+func getContainers() ([]map[string]interface{}, error) {
+	args := []string{"ps", "--all", "--sort", "names", "--filter", "label=com.github.debarshiray.toolbox=true", "--format", "json"}
+	cmd := exec.Command("podman", args...)
+	output, err := cmd.Output()
+
+	var containers_debarshi []map[string]interface{}
+
+	err = json.Unmarshal(output, &containers_debarshi)
+	if err != nil {
+		return nil, err
+	}
+
+	args = []string{"ps", "--all", "--sort", "names", "--filter", "label=com.github.containers.toolbox=true", "--format", "json"}
+	cmd = exec.Command("podman", args...)
+	output, err = cmd.Output()
+
+	var containers_containers []map[string]interface{}
+
+	err = json.Unmarshal(output, &containers_containers)
+	if err != nil {
+		panic(err)
+	}
+
+	containers := utils.JoinJson("ID", containers_debarshi, containers_containers)
+	containers = utils.SortJson(containers, "Names", false)
+	return containers, err
+}
+
+func getImages() ([]map[string]interface{}, error) {
+	args := []string{"images", "--all", "--sort", "names", "--filter", "label=com.github.debarshiray.toolbox=true", "--format", "json"}
+	cmd := exec.Command("podman", args...)
+	output, err := cmd.Output()
+
+	var imgs_debarshi []map[string]interface{}
+
+	err = json.Unmarshal(output, &imgs_debarshi)
+	if err != nil {
+		return nil, err
+	}
+
+	args = []string{"images", "--all", "--sort", "names", "--filter", "label=com.github.containers.toolbox=true", "--format", "json"}
+	cmd = exec.Command("podman", args...)
+	output, err = cmd.Output()
+
+	var imgs_containers []map[string]interface{}
+
+	err = json.Unmarshal(output, &imgs_containers)
+	if err != nil {
+		panic(err)
+	}
+
+	images := utils.JoinJson("id", imgs_debarshi, imgs_containers)
+	images = utils.SortJson(images, "names", true)
+
+	return images, err
+}
+
+func outputList(images, containers []map[string]interface{}) error {
+	if len(images) != 0 {
+		w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+		fmt.Fprintf(w, "%s\t%s\t%s\n", "IMAGE ID", "IMAGE NAME", "CREATED")
+		for _, image := range images {
+			id := image["id"].(string)[:12]
+			name := image["names"].([]interface{})[0]
+			created := image["created"]
+			fmt.Fprintf(w, "%s\t%s\t%s\n", id, name, created)
+		}
+		w.Flush()
+	}
+
+	if len(images) != 0 && len(containers) != 0 {
+		fmt.Println()
+	}
+
+	if len(containers) != 0 {
+		w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+		fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\n", "CONTAINER ID", "CONTAINER NAME", "CREATED", "STATUS", "IMAGE NAME")
+		for _, container := range containers {
+			id := container["ID"].(string)[:12]
+			name := container["Names"]
+			created := container["Created"]
+			status := container["Status"]
+			imageName := container["Image"]
+			fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\n", id, name, created, status, imageName)
+		}
+		w.Flush()
+	}
+	return nil
 }
