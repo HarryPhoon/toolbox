099ab1170664f58593f8b13eddbe6c88eb5d1067
Harry Míchal <harrymichal@seznam.cz>
2019-12-12 16:06:01 +0100

    Add more helper functions
    These functions are needed in several parts of code. It's good to have
    them in an separate utils package.

--- utils/common.go
+++ utils/common.go
@@ -1,7 +1,13 @@
 package utils
 
 import (
+	"fmt"
+	"regexp"
 	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/sirupsen/logrus"
 )
 
 const (
@@ -13,6 +19,54 @@ func ShortID(id string) string {
 		return id[:idTruncLength]
 	}
 	return id
+}
+
+// ReferenceCanBeID checks if the provided text matches a format for an ID
+func ReferenceCanBeID(text string) bool {
+	matched, err := regexp.MatchString(`^[a-f0-9]\{6,64\}$`, text)
+	if err != nil {
+		logrus.Error(err)
+	}
+	return matched
+}
+
+// ReferenceHasDomain checks if the provided text has a domain definition in it
+func ReferenceHasDomain(text string) bool {
+	i := strings.IndexRune(text, '/')
+	if i == -1 {
+		return false
+	}
+
+	// A domain should contain a top level domain name. An exception is 'localhost'
+	if strings.ContainsAny(text[:i], ".:") && text[:i] != "localhost" {
+		return false
+	}
+
+	return true
+}
+
+// NumberPrompt creates an interactive prompt that expects and returns an integer
+func NumberPrompt(defaultValue int, min int, max int, prompt string) int {
+	var tmp string
+	var response int = 0
+
+	for true {
+		fmt.Printf("%s (%d to abort) [%d-%d]: ", prompt, defaultValue, min, max)
+		fmt.Scanf("%s", &tmp)
+
+		tmpresponse, err := strconv.ParseInt(tmp, 10, 0)
+		if err != nil {
+			continue
+		}
+
+		response = int(tmpresponse)
+
+		if response >= min && response <= max {
+			break
+		}
+	}
+
+	return response
 }
 
 func JoinJSON(joinkey string, maps ...[]map[string]interface{}) []map[string]interface{} {
