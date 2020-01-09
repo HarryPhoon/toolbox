70227db4c581d08343aa1f9b663f656bdc24fa1c
Harry Míchal <harrymichal@seznam.cz>
2020-01-09 13:29:26 +0100

    Add wrapper for getting local images and containers
    In the code are several parts that need to get all the present images
    and containers. This simplifies those parts.

--- utils/podman.go
+++ utils/podman.go
@@ -1,11 +1,60 @@
 package utils
 
 import (
+	"encoding/json"
 	"os/exec"
 	"syscall"
 
 	"github.com/pkg/errors"
 )
+
+// GetContainers is a wrapper function around `podman ps --format json` command.
+//
+// Parameter args accepts an array of strings to be passed to the wrapped command (eg. ["-a", "--filter", "123"]).
+//
+// Returned value is a slice of dynamically unmarshalled json, so it needs to be treated properly.
+//
+// If a problem happens during execution, first argument is nil and second argument holds the error message.
+func GetContainers(args ...string) ([]map[string]interface{}, error) {
+	args = append([]string{"ps", "--format", "json"}, args...)
+	output, err := PodmanOutput(args...)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	var containers []map[string]interface{}
+
+	err = json.Unmarshal(output, &containers)
+	if err != nil {
+		return nil, errors.Wrap(err, "Problem while unmarshalling json with toolbox containers")
+	}
+
+	return containers, nil
+}
+
+// GetImages is a wrapper function around `podman images --format json` command.
+//
+// Parameter args accepts an array of strings to be passed to the wrapped command (eg. ["-a", "--filter", "123"]).
+//
+// Returned value is a slice of dynamically unmarshalled json, so it needs to be treated properly.
+//
+// If a problem happens during execution, first argument is nil and second argument holds the error message.
+func GetImages(args ...string) ([]map[string]interface{}, error) {
+	args = append([]string{"images", "--format", "json"}, args...)
+	output, err := PodmanOutput(args...)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	var images []map[string]interface{}
+
+	err = json.Unmarshal(output, &images)
+	if err != nil {
+		return nil, errors.Wrap(err, "Problem while unmarshalling json with toolbox images")
+	}
+
+	return images, nil
+}
 
 func PodmanOutput(args ...string) ([]byte, error) {
 	cmd := exec.Command("podman", args...)
