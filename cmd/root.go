1665939776e1ec8e3c515b7c3f97f77dd49fc044
Harry Míchal <harrymichal@seznam.cz>
2020-02-21 15:11:22 +0100

    Add handling for Podman migration
    When Podman updates it is sometimes necessary to call the `podman system
    migrate` to ensure that the existing containers will run properly with
    the new version.
    
    Toolbox checks the need to update by storing the latest version of
    Podman used by Toolbox in ~/.config/toolbox/podman-system-migrate file.
    
    The function doesn't have to be invoked when the 'init-container'
    command is used or if Toolbox is ran in a container (because the
    container should already be alright, right?).

--- cmd/root.go
+++ cmd/root.go
@@ -16,14 +16,17 @@ limitations under the License.
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"path/filepath"
 
+	"github.com/containers/toolbox/pkg/podman"
+	"github.com/mitchellh/go-homedir"
+
 	"github.com/containers/toolbox/pkg/utils"
 	"github.com/spf13/cobra"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
@@ -78,6 +81,15 @@ developing and debugging software that runs fully unprivileged using Podman.`,
 			// Set the toolbox runtime directory
 			viper.Set("TOOLBOX_RUNTIME_DIRECTORY", fmt.Sprintf("%s/toolbox", viper.GetString("XDG_RUNTIME_DIR")))
 			logrus.Debugf("Toolbox runtime directory is %s", viper.GetString("TOOLBOX_RUNTIME_DIRECTORY"))
+
+			// Check if it is needed to migrate to a new Podman version
+			// This doesn't have to be done in a container
+			if calledCmd != "init-container" && !inContainer {
+				err = migrate()
+				if err != nil {
+					logrus.Fatal(err)
+				}
+			}
 
 			// Here we could place some logic to take care of invoing toolbox or other commands from within container by piping them to the host
 			// FIXME
@@ -151,6 +163,66 @@ func setUpLoggers() error {
 	}
 
 	logrus.SetLevel(lvl)
+
+	return nil
+}
+
+func migrate() error {
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		return fmt.Errorf("Could not get the user config directory: %w", err)
+	}
+	toolboxConfigDir := fmt.Sprintf("%s/toolbox", configDir)
+	migrateStampPath := fmt.Sprintf("%s/podman-system-migrate", configDir)
+	logrus.Debugf("Toolbox config directory is %s", toolboxConfigDir)
+
+	podmanVersion, err := podman.GetVersion()
+	if err != nil {
+		return fmt.Errorf("Could not get the version of Podman: %w", err)
+	}
+	logrus.Debugf("Current Podman version is %s", podmanVersion)
+
+	err = os.MkdirAll(toolboxConfigDir, 0664)
+	if err != nil {
+		return fmt.Errorf("Configuration directory not created: %w", err)
+	}
+
+	migrateStampFile, err := os.OpenFile(migrateStampPath, os.O_RDWR|os.O_CREATE, 0664)
+	if err != nil {
+		return fmt.Errorf("Could not open/create file '%s': %w", migrateStampPath, err)
+	}
+	defer migrateStampFile.Close()
+
+	podmanVersionOld := ""
+	scanner := bufio.NewScanner(migrateStampFile)
+	if scanner.Scan() {
+		podmanVersionOld = scanner.Text()
+	}
+
+	if podmanVersionOld != "" {
+		logrus.Debugf("Old Podman version is %s", podmanVersionOld)
+		versionComp := podman.CheckVersion(podmanVersionOld)
+		if versionComp == 0 {
+			logrus.Debugf("Migration not needed: Podman version %s is unchanged", podmanVersion)
+			return nil
+		} else if versionComp > 0 {
+			logrus.Debugf("Migration not needed: Podman version %s is old", podmanVersion)
+			return nil
+		} else {
+			logrus.Debugf("Migration needed: Podman version %s is new", podmanVersion)
+			err = podman.CmdRun("system", "migrate")
+			if err != nil {
+				return fmt.Errorf("Unable to migrate containers: %w", err)
+			}
+			logrus.Debugf("Migration to Podman version %s was ok", podmanVersion)
+		}
+	}
+
+	logrus.Infof("Updating Podman version in '%s'", migrateStampPath)
+	_, err = migrateStampFile.WriteString(podmanVersion)
+	if err != nil {
+		return fmt.Errorf("Could not update version of Podman in '%s': %w", migrateStampPath, err)
+	}
 
 	return nil
 }
